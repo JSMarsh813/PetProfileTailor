fb8e4ea2de043f8b26e145811dabc9f5a0a54181
JSMarsh813 <janetspellman13@gmail.com>
2023-03-25 09:49:26 -0700

    checking for improvement on vercel

--- pages/api/names/swr/swr.js
+++ pages/api/names/swr/swr.js
@@ -5,8 +5,8 @@ import Tags from "../../../../models/NameTag";
 export default async function handler(req, res) {
   const method = req.method;
   const { page, limit, sortinglogicstring } = req.query;
-  // let skipvalue = parseInt((page - 1) * limit);
-  // let limitvalue = parseInt(limit);
+  let skipvalue = parseInt((page - 1) * limit);
+  let limitvalue = parseInt(limit);
 
   //https://stackoverflow.com/questions/70751313/how-can-i-pass-a-variable-in-sort-funtcion-of-mongobd
   // let sortlogic = {};
@@ -42,29 +42,29 @@ export default async function handler(req, res) {
           },
         },
         { $unwind: "$createdby" },
-        {
-          $lookup: {
-            from: "nametags",
-            localField: "tags",
-            foreignField: "_id",
-            as: "tags",
-          },
-        },
-        {
-          $project: {
-            name: 1,
-            description: 1,
-            tags: { tag: 1 },
-            comments: 1,
-            createdby: {
-              name: 1,
-              profilename: 1,
-              profileimage: 1,
-            },
-            likedby: 1,
-            length: { $size: "$likedby" },
-          },
-        },
+        // {
+        //   $lookup: {
+        //     from: "nametags",
+        //     localField: "tags",
+        //     foreignField: "_id",
+        //     as: "tags",
+        //   },
+        // },
+        // {
+        //   $project: {
+        //     name: 1,
+        //     description: 1,
+        //     tags: { tag: 1 },
+        //     comments: 1,
+        //     createdby: {
+        //       name: 1,
+        //       profilename: 1,
+        //       profileimage: 1,
+        //     },
+        //     likedby: 1,
+        //     length: { $size: "$likedby" },
+        //   },
+        // },
       ]);
 
       res.status(200).json(individualNames);
