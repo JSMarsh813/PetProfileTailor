7c4c5fda595d62cc324997f50bf0cb077b07e4bf
JSMarsh813 <janetspellman13@gmail.com>
2023-03-25 10:57:35 -0700

    readding second lookup

--- pages/api/names/swr/swr.js
+++ pages/api/names/swr/swr.js
@@ -43,29 +43,29 @@ export default async function handler(req, res) {
           },
         },
         { $unwind: "$createdby" },
-        // {
-        //   $lookup: {
-        //     from: "nametags",
-        //     localField: "tags",
-        //     foreignField: "_id",
-        //     as: "tags",
-        //   },
-        // },
-        // {
-        //   $project: {
-        //     name: 1,
-        //     description: 1,
-        //     tags: { tag: 1 },
-        //     comments: 1,
-        //     createdby: {
-        //       name: 1,
-        //       profilename: 1,
-        //       profileimage: 1,
-        //     },
-        //     likedby: 1,
-        //     length: { $size: "$likedby" },
-        //   },
-        // },
+        {
+          $lookup: {
+            from: "nametags",
+            localField: "tags",
+            foreignField: "_id",
+            as: "tags",
+          },
+        },
+        {
+          $project: {
+            name: 1,
+            description: 1,
+            tags: { tag: 1 },
+            comments: 1,
+            createdby: {
+              name: 1,
+              profilename: 1,
+              profileimage: 1,
+            },
+            likedby: 1,
+            length: { $size: "$likedby" },
+          },
+        },
       ]);
 
       res.status(200).json(individualNames);
