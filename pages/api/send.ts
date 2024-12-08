016da024cf0dd56770e196c38880efba1ef77210
Janet-Spellman <janetspellman13@gmail.com>
2024-12-07 21:47:06 -0800

    moving to resend about to use codemod to change link legacy behavior after next 12 to 13 upgrade

---
+++ pages/api/send.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { EmailTemplate } from "../../components/email-template";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const { data, error } = await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
+    to: ["janetspellman13@gmail.com"],
+    subject: "Hello world",
+    react: EmailTemplate({ firstName: "John" }),
+  });
+
+  if (error) {
+    return res.status(400).json(error);
+  }
+
+  res.status(200).json(data);
+};
