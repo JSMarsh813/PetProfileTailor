e9a741fa01f597cc42bf475730aa1c6fff3b823e
Janet-Spellman <janetspellman13@gmail.com>
2024-12-20 02:37:44 -0800

    commented out the sample resend email file that was causing build issues

--- pages/api/auth/lib/resend.ts
+++ pages/api/auth/lib/resend.ts
@@ -1,24 +1,23 @@
-import { Resend } from "resend";
-import type { NextApiRequest, NextApiResponse } from "next";
-import { SendVerificationRequestParams } from "next-auth/providers";
-import { EmailTemplate } from "../../../../components/EmailTemplates/email-header-template";
+// import { Resend } from "resend";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import { SendVerificationRequestParams } from "next-auth/providers";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendVerificationRequest(
-  req: NextApiRequest,
-  res: NextApiResponse,
-) {
-  const { data, error } = await resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "janetspellman13@gmail.com",
-    subject: "Hello world",
-    react: EmailTemplate({ firstName: "John" }),
-  });
+// export async function sendVerificationRequest(
+//   req: NextApiRequest,
+//   res: NextApiResponse,
+// ) {
+//   const { data, error } = await resend.emails.send({
+//     from: "onboarding@resend.dev",
+//     to: "janetspellman13@gmail.com",
+//     subject: "Hello world",
+//     react: EmailTemplate({ firstName: "John" }),
+//   });
 
-  if (error) {
-    return res.status(400).json(error);
-  }
+//   if (error) {
+//     return res.status(400).json(error);
+//   }
 
-  return res.status(200).json(data);
-}
+//   return res.status(200).json(data);
+// }
