f31e20f1d7c5951b4fd0fb26ddca8ad884914a5e
Janet-Spellman <janetspellman13@gmail.com>
2025-01-20 01:46:28 -0800

    fixed pagination sizing for mobile

--- components/ShowingListOfContent/pagination.js
+++ components/ShowingListOfContent/pagination.js
@@ -43,48 +43,57 @@ export default function Pagination({
   };
 
   return (
-    <section className="pagination-navigation flex flex-row min-w-0 lg:flex-content bg-violet-800 place-content-between text-violet-900 font-bold pt-2 sm:border-x-4 border-darkPurple">
-      {/* items per page */}
-      <div className="my-auto">
-        <select
-          id="per-page"
-          className="bg-violet-200  ml-2"
-          value={itemsPerPage}
-          onChange={(e) => setItemsPerPageFunction(e.target.value)}
-        >
-          <option value="5">5</option>
-          <option value="15">15</option>
-          <option value="30">30</option>
-          <option value="60">60</option>
-        </select>
-        <label
-          className="text-white ml-2"
-          htmlFor="per-page"
-        >
-          Per Page
-        </label>
+    <section className="pagination-navigation grid grid-rows-1 min-w-0  bg-violet-800 text-violet-900 font-bold pt-2 sm:border-x-4 border-darkPurple">
+      {/* sorting logic*/}
+      <div className="inline my-auto pb-3 ">
+        {/* wrapping the selects in sections & inline-block keeps the per page and sort by labels from wrapping weirdly at smaller sizes */}
 
-        <select
-          id="per-page"
-          className="bg-violet-200  ml-2"
-          onChange={(e) => setSortingLogicFunction(e.target.value)}
-        >
-          <option value="_id,-1">Newest</option>
-          <option value="_id,1">Oldest </option>
-          <option value="likedbylength,-1">Most Liked</option>
-          <option value="likedbylength,1">Least Liked</option>
-        </select>
-        <label
-          className="text-white ml-2"
-          htmlFor="per-page"
-        >
-          Sort by
-        </label>
+        {/* Per page */}
+        <section className="inline-block">
+          <select
+            id="per-page"
+            className="bg-violet-200  ml-2"
+            value={itemsPerPage}
+            onChange={(e) => setItemsPerPageFunction(e.target.value)}
+          >
+            <option value="5">5</option>
+            <option value="15">15</option>
+            <option value="30">30</option>
+            <option value="60">60</option>
+          </select>
+          <label
+            className="text-white ml-2"
+            htmlFor="per-page"
+          >
+            Per Page
+          </label>
+        </section>
+        {/* sort by */}
+        <section className="inline-block">
+          <select
+            id="per-page"
+            className="bg-violet-200  ml-2 p-2"
+            onChange={(e) => setSortingLogicFunction(e.target.value)}
+          >
+            <option value="_id,-1">Newest</option>
+            <option value="_id,1">Oldest </option>
+            <option value="likedbylength,-1">Most Liked</option>
+            <option value="likedbylength,1">Least Liked</option>
+          </select>
+
+          <label
+            className="text-white ml-2"
+            htmlFor="per-page"
+          >
+            Sort by
+          </label>
+        </section>
       </div>
 
-      <div className="flex-row md:flex m-2">
+      {/* PAGINATION ARROWS */}
+      <div className=" flex justify-center mb-3 border-t-2 b-white pt-3">
         <button
-          className="prevpage"
+          className="prevpage "
           aria-label="prevpage"
           disabled={page == 1}
           type="submit"
@@ -97,38 +106,36 @@ export default function Pagination({
           />
         </button>
 
-        <div className="lg:w-full">
-          {arrayOfPageNumbers.map((number) => {
-            if (number > page + 4 || number < page - 5) {
-              return;
-            }
+        {arrayOfPageNumbers.map((number) => {
+          if (number > page + 2 || number < page - 3) {
+            return;
+          }
 
-            return (
-              <GeneralButton
-                text={number}
-                key={number}
-                className={`py-1 px-4 mx-2 mt-1 ${
-                  number == page && "bg-indigo-300 border-indigo-600"
-                }`}
-                onClick={() =>
-                  number == lastPageNumber
-                    ? clickOnLastNumber(number)
-                    : setPageFunction(number)
-                }
-              />
-            );
-          })}
-        </div>
+          return (
+            <GeneralButton
+              text={number}
+              key={number}
+              className={`py-1 px-4 mx-2 mt-1 ${
+                number == page && "bg-indigo-300 border-indigo-600"
+              }`}
+              onClick={() =>
+                number == lastPageNumber
+                  ? clickOnLastNumber(number)
+                  : setPageFunction(number)
+              }
+            />
+          );
+        })}
 
         <button
           aria-label="nextpage"
-          className="nextpage"
+          className="nextpage aligncenter"
           type="submit"
           onClick={() => lastPageHandler(page)}
         >
           <FontAwesomeIcon
             icon={faChevronCircleRight}
-            className="text-3xl mt-2 md:mt-0"
+            className="text-3xl mt-2 md:mt-0 "
             color={`${page < filteredListLastPage ? "yellow" : "grey"}`}
           />
         </button>
