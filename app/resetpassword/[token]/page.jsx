e9b7a85bd76dff9f3396b80afb997e0b9658ed0e
Janet-Spellman <janetspellman13@gmail.com>
2025-09-13 23:38:05 -0700

    refactoring: reset token page moved to app

---
+++ app/resetpassword/[token]/page.jsx
@@ -0,0 +1,16 @@
+import dbConnect from "@utils/db";
+import { getServerSession } from "next-auth";
+import { serverAuthOptions } from "@/lib/auth";
+import ResetPassword from "@/components/ResetPassword";
+
+export default async function ResetPasswordPage({ params }) {
+  //allows us to grab the dynamic value from the url
+  const session = await getServerSession(serverAuthOptions);
+
+  if (session) {
+    return redirect("/dashboard");
+  }
+
+  const { token } = params;
+  return <ResetPassword token={token} />;
+}
