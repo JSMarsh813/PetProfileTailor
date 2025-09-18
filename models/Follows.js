36cb2158cf7194f132b0866ef35c42d63be81633
Janet-Spellman <janetspellman13@gmail.com>
2025-09-17 21:59:05 -0700

    feat: added a follows model

---
+++ models/Follows.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const { ObjectId } = mongoose.Schema.Types;
+
+const FollowsSchema = new mongoose.Schema(
+  {
+    userId: { type: ObjectId, ref: "Users", required: true },
+    followedBy: { type: ObjectId, ref: "Users", required: true },
+  },
+  { timestamps: true },
+);
+
+// prevent duplicate likes
+FollowsSchema.index({ userId: 1, followedBy: 1 }, { unique: true });
+// FollowsSchema.index
+// An index in MongoDB is like a sorted lookup table for faster queries.
+// Instead of scanning the entire collection to find matches, MongoDB jumps straight to the right place in the index.
+// In this case, you’re building an index on two fields: userId and nameId.
+
+//{ userId: 1, nameId: 1 }
+//“Make an index where documents are sorted by userId first, then by nameId.”
+// since we'd need to return a list of names that match the userID for the dashboard, so userID is the bigger priority
+
+// { unique: true }
+//makes the combination of userId + nameId unique across the entire collection, so it can only be liked once
+
+const Follows =
+  mongoose.models.Follows ||
+  mongoose.model("Follows", FollowsSchema, "follows");
+
+export default Follows;
