8785bb1f5f05ccf0e3ded0ea0e0ba6bd95917438
Janet-Spellman <janetspellman13@gmail.com>
2025-10-02 00:56:49 -0700

    bug: added import for warning message component

--- components/AddingNewData/addingdescription.jsx
+++ components/AddingNewData/addingdescription.jsx
@@ -3,6 +3,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Image from "next/image";
+import WarningMessage from "../ReusableSmallComponents/buttons/WarningMessage";
 
 import { toast } from "react-toastify";
 
